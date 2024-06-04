bf92615a25ab954002b284e2ebfb516f0ce6a918
Deepak Bhagat <149673145+deepak-bhagat-iitr@users.noreply.github.com>
2024-06-05 01:47:03 +0530

    Update action.js

--- src/components/action.js
+++ src/components/action.js
@@ -164,16 +164,16 @@ const Action = ({ status, serialNumber, onClose }) => {
 
         <div className="pt-4 border-t">
           <h3 className="text-lg font-medium text-gray-800">Report of IPAC</h3>
-          <div className="mt-2">
-            <input
-              type="text"
+         <div className="mt-2">
+            <textarea
               id="comment"
               className="block w-full p-2.5 text-sm bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Report of IPAC"
               value={comment}
               onChange={handleCommentChange}
               required
-            />
+              rows="3"
+            ></textarea>
             <button
               type="button"
               className="px-5 py-2 mt-3 mb-2 text-sm font-medium text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-green-300 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
